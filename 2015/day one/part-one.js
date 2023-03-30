cef086b47ffe43f89994e4cc4bb69adfa41ed25a
Wolfhaize <92583384+Wolfhaize@users.noreply.github.com>
2023-03-30 13:58:21 +0300

    Update and rename part-one.js to 2015/day one/part-one.js

--- 2015/day one/part-one.js
+++ 2015/day one/part-one.js
@@ -1,10 +1,10 @@
 const input = require('./input');//import input from input.js
 
-const GO_UP = '(';
-const GO_DOWN = ')';
+const UP = '(';
+const DOWN = ')';
 
 let final_floor = input
-    .map(v => (v === GO_UP ? 1 : -1)) //map brackets to 1 or -1
+    .map(v => (v === UP ? 1 : -1)) //map brackets to 1 or -1
     .reduce((a, b) => a + b, 0);// return the sum of all the elements in an array
 
 console.log(`Santa ends up on floor "${final_floor}"`);
