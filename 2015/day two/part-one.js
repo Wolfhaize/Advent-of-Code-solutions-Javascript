ec6a7f27d667733279ed7a8255c12317ff825093
Wolfhaize <92583384+Wolfhaize@users.noreply.github.com>
2023-03-30 14:09:54 +0300

    Add files via upload

---
+++ 2015/day two/part-one.js
@@ -0,0 +1,21 @@
+const input = require('./input');
+
+let input_areas =  input.map(box => {
+    let [s1, s2, s3] = box.split('x').map(n => +n);
+
+    //`2*l*w + 2*w*h + 2*h*l`
+
+    let a = s1 * s2;
+    let b = s1 * s3;
+    let c = s2 * s3;
+
+    let min_side = Math.min((a, b, c), 2);
+    
+
+
+    return (a * 2) + (b * 2) + (c * 2) + min_side;
+});
+
+let total_square_feet = input_areas.reduce((a, b) => a + b, 0);
+
+console.log(`Elves will need to order ${total_square_feet} square feet of wrapping paper`);
